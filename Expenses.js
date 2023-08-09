98eae4006b301ef59f2c81966d67294f54c7fa7e
Raj9644 <99965980+Raj9644@users.noreply.github.com>
2023-08-09 12:06:03 +0530

    Update Expenses.js

--- Expenses.js
+++ Expenses.js
@@ -12,6 +12,32 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+
+    if (filteredExpenses.length === 1) {
+      expensesContent = (
+        <div>
+          {expensesContent}
+          <p>Only a single expense here. Please add more...</p>
+        </div>
+      );
+    }
+  }
+
   return (
     <div>
       <Card className='expenses'>
@@ -19,14 +45,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
